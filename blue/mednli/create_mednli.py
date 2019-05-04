8ec7724925c04e462cc3f79a9ed25b462c4b7b6b
Yifan Peng <yifan.peng@nih.gov>
2019-05-03 21:08:01 -0400

    simplify mednli

--- blue/mednli/create_mednli.py
+++ blue/mednli/create_mednli.py
@@ -1,36 +1,31 @@
-"""
-Usage:
-    create_medni <jsonl> <dest>
-
-Options:
-    <jsonl>
-    <dest>
-"""
-
 import csv
 import json
 
-import docopt
+import fire
 import tqdm
+from pathlib import Path
 
 from blue import pstring
 
-labels = ['contradiction', 'entailment', 'neutral']
-
 
 def convert(src, dest):
-    labels = set()
     with open(src, encoding='utf8') as fin, open(dest, 'w', encoding='utf8') as fout:
         writer = csv.writer(fout, delimiter='\t', lineterminator='\n')
-        writer.writerow(['label', 'id', 'sentence1', 'sentence2'])
+        writer.writerow(['index', 'sentence1', 'sentence2', 'label'])
         for line in tqdm.tqdm(fin):
             line = pstring.printable(line, greeklish=True)
             obj = json.loads(line)
-            writer.writerow([obj['gold_label'], obj['pairID'], obj['sentence1'], obj['sentence2']])
-            labels.add(obj['gold_label'])
-    print(sorted(labels))
+            writer.writerow([obj['pairID'], obj['sentence1'], obj['sentence2'], obj['gold_label']])
+
+
+def create_mednli(mednli_dir):
+    mednli_dir = Path(mednli_dir)
+    for src_name, dst_name in zip(['mli_train_v1.jsonl', 'mli_dev_v1.jsonl', 'mli_test_v1.jsonl'],
+                                  ['train.tsv', 'dev.tsv', 'test.tsv']):
+        source = mednli_dir / src_name
+        dest = mednli_dir / dst_name
+        convert(source, dest)
 
 
 if __name__ == '__main__':
-    argv = docopt.docopt(__doc__)
-    convert(argv['jsonl'], argv['dest'])
+    fire.Fire(create_mednli)
